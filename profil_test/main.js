00dd16cb2e73c7f38b7df9db2a46ee536d605590
hheerriinn <hheerriinn@gmail.com>
2023-11-15 10:42:53 +0900

    231115

--- profil_test/main.js
+++ profil_test/main.js
@@ -1,3 +1,21 @@
+window.onload = function(){
+   let picture = ["images/bgbgbg.jpg", "images/bg4.jpg",
+                  "images/bg3.jpg"];
+   let picIdx = 0;
+
+   showSlide();
+   
+   function showSlide(){
+      //이미지 경로 - src 속성 사용
+      document.getElementById("pic").src = picture[picIdx];
+      picIdx += 1;
+      if(picIdx == picture.length) {
+         picIdx = 0;
+      }
+      setTimeout(showSlide, 2000);
+   }
+}
+
 setInterval(myWatch, 1000);
 
 function myWatch(){
